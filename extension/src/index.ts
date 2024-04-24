3430b6c786a73a332fd8ca2723ccb4388a4af4cf
imrany <imranmat254@gmail.com>
2024-04-24 19:27:08 +0300

    using WebSocket API to send and receive data from the server

--- extension/src/index.ts
+++ extension/src/index.ts
@@ -1,33 +1,75 @@
+const socket=new WebSocket("ws://localhost:8000");
+socket.onopen=(event:any)=>{
+    console.log("WebSocket connected");
+    socket.send("Hello, websocket");
+}
+
+socket.onmessage = (event:any)=>{
+    let message=event.data;
+    
+    // Check if the message is a Blob
+    if (message instanceof Blob) {
+        var reader = new FileReader();
+        reader.onload = ()=>{
+            let text = reader.result;
+            console.log(text)
+        };
+        reader.readAsText(message);
+    } else {
+        // If the message is not a Blob, treat it as a string
+        console.log(message)
+    }
+};
+
+socket.onclose = (event:any)=>{
+  console.log('WebSocket closed.');
+};
+
+socket.onerror = (error:any)=>{
+  console.error('WebSocket error:', error);
+};
+
 function onAnchorClick(event:any) {
-    chrome.tabs.create({
-      selected: true,
-      url: event.srcElement.href
-    });
-    return false;
-  }
-  
-  // Search history to find up to ten links that a user has typed in,
-  // and show those links in a popup.
-  function buildTypedUrlList() {
-    // To look for history items visited in the last week,
-    // subtract a week of microseconds from the current time.
-    let microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
-    let oneWeekAgo = new Date().getTime() - microsecondsPerWeek;
-  
-    // Track the number of callbacks from chrome.history.getVisits()
-    // that we expect to get.  When it reaches zero, we have all results.
-    chrome.history.search(
-      {
-        text: '', // Return every history item....
-        startTime: oneWeekAgo // that was accessed less than one week ago.
-      },
-      function (historyItems:any) {
-        console.log(historyItems)
-      }
-    );
-  }
-  
-  document.addEventListener('DOMContentLoaded', function () {
-    buildTypedUrlList()
+  chrome.tabs.create({
+    selected: true,
+    url: event.srcElement.href
   });
+  return false;
+}
+  
+// Search history to find up to ten links that a user has typed in,
+// and show those links in a popup.
+function buildTypedUrlList() {
+  // To look for history items visited in the last week,
+  // subtract a week of microseconds from the current time.
+  // let microsecondsPerWeek = 1000 * 60 * 60 * 24 * 7;
+  let microsecondsPerDay = 1000 * 60 * 60 * 24;
+  let oneDayAgo = new Date().getTime() - microsecondsPerDay;
+
+  // Track the number of callbacks from chrome.history.getVisits()
+  // that we expect to get.  When it reaches zero, we have all results.
+  chrome.history.search(
+    {
+      text: '', // Return every history item....
+      startTime: oneDayAgo // that was accessed less than one week ago.
+    },
+    function (historyItems:any) {
+      console.log(historyItems)
+    }
+  );
+}
+
+setInterval(()=>{
+  // buildTypedUrlList()
+},500);
+
+async function fetchData() {
+  const response = await fetch('https://google.com')
+  console.log(response);
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+  buildTypedUrlList()
+  fetchData()
+});
   
